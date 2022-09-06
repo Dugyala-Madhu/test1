34202636926e2dd195c5c3b0e3a66f6b8d492627
Dugyala-Madhu <112916889+Dugyala-Madhu@users.noreply.github.com>
2022-09-06 08:37:56 +0530

    Add files via upload

---
+++ com.mindtree.exception/src/com/mindtree/exception/Insufficentfundsexception.java
@@ -0,0 +1,16 @@
+package com.mindtree.exception;
+
+public class Insufficentfundsexception extends Exception {
+	
+	Insufficentfundsexception()
+	{
+		super();
+	
+	}
+	Insufficentfundsexception(String msg){
+		
+		super (msg);
+	}
+	
+
+}
---
+++ com.mindtree.exception/src/com/mindtree/exception/InvalidAmountexception.java
@@ -0,0 +1,13 @@
+package com.mindtree.exception;
+
+public class InvalidAmountexception extends Exception  {
+	
+	InvalidAmountexception(){
+		super();
+		
+	}
+	InvalidAmountexception(String msg){
+		super (msg);
+	}
+
+}
